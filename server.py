8d6e0a8dbb314b4cc4722a539a1e6170c5a71d6f
pr <peter@peter.net>
2017-11-16 19:02:41 -0500

    oh

--- server.py
+++ server.py
@@ -65,5 +65,5 @@ def index_post():
 
 if __name__ == '__main__':
     print('APP STARTED!!')
-    socketio.run(app)
-    app.run(host='0.0.0.0', port='PORT')
+    #socketio.run(app)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
